b3037a215746ade22fab0a12955815638ea87540
FnEsc <1033742389@qq.com>
2021-09-24 17:46:09 +0800

    20210924

--- Go语言圣经/ch5 函数/ch5.go
+++ Go语言圣经/ch5 函数/ch5.go
@@ -19,6 +19,7 @@ func main() {
 	// 在函数声明时候明确声明的多个返回值，并且在或函数内变量有明确的变量名，即可省略 return 的多个名字
 
 	// 5.4 错误
+	// http://books.studygolang.com/gopl-zh/ch5/ch5-04.html
 }
 
 func f(i, j, k int, s, t string) { /* 函数返回值列表可省略 */ }
