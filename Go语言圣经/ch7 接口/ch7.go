0cf2298bf81714cb2639de5b321ec129d8e42400
FnEsc <1033742389@qq.com>
2021-09-29 17:47:23 +0800

    20210929

--- Go语言圣经/ch7 接口/ch7.go
+++ Go语言圣经/ch7 接口/ch7.go
@@ -1,1 +1,52 @@
 // http://books.studygolang.com/gopl-zh/ch7/ch7.html
+// 接口的动态类型和动态值
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var period = flag.Duration("period", 1*time.Second, "sleep period")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...", *period)
+	time.Sleep(*period)
+	fmt.Println()
+
+	db := database{"shoes": 50, "socks": 5}
+	log.Fatal(http.ListenAndServe("localhost:8000", db))
+
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+	default:
+		msg := fmt.Sprintf("no such page: %s\n", req.URL)
+		http.Error(w, msg, http.StatusNotFound) // 404
+	}
+
+}
